e778c905043ac8f6c5e2124a5d7ab53a2d9fa8e1
Wilson Lin <code@wilsonl.in>
2025-12-17 15:19:52 -0500

    Silence unused underline variable in display list test

--- tests/display_list_renderer_test.rs
+++ tests/display_list_renderer_test.rs
@@ -395,7 +395,7 @@ fn text_decoration_currentcolor_resolves_in_display_list() {
         })
         .expect("decoration item");
 
-    let underline = deco
+    let _underline = deco
         .decorations
         .first()
         .and_then(|d| d.underline.as_ref())
