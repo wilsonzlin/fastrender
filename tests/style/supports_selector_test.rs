20ba7d8bb8663db7eedcc06820a8b9e6fb19f12d
Wilson Lin <code@wilsonl.in>
2025-12-17 07:56:20 -0500

    Add @supports selector() regressions

---
+++ tests/style/supports_selector_test.rs
@@ -0,0 +1,28 @@
+use fastrender::css::parser::parse_stylesheet;
+use fastrender::style::cascade::apply_styles_with_media;
+use fastrender::style::media::MediaContext;
+use fastrender::{dom, DomNode};
+
+fn display(node: &DomNode) -> String {
+    node.styles.display.to_string()
+}
+
+#[test]
+fn supports_selector_true_for_supported_selector() {
+    let dom = dom::parse_html(r#"<div class="foo"></div>"#).unwrap();
+    let css = r#"@supports selector(.foo) { div { display: inline; } }"#;
+    let stylesheet = parse_stylesheet("supports", css, None).unwrap();
+    let styled = apply_styles_with_media(&dom, &stylesheet, &MediaContext::screen(800.0, 600.0));
+
+    assert_eq!(display(&styled.children[0]), "inline");
+}
+
+#[test]
+fn supports_selector_false_for_unsupported_selector() {
+    let dom = dom::parse_html(r#"<div></div>"#).unwrap();
+    let css = r#"@supports selector(:has(div)) { div { display: inline; } }"#;
+    let stylesheet = parse_stylesheet("supports", css, None).unwrap();
+    let styled = apply_styles_with_media(&dom, &stylesheet, &MediaContext::screen(800.0, 600.0));
+
+    assert_eq!(display(&styled.children[0]), "block");
+}
