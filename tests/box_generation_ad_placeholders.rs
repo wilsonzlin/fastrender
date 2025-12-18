0ee5b0953a0329454f822a91d5b0609f4506958d
Wilson Lin <code@wilsonl.in>
2025-12-18 02:26:53 -0500

    Add regression skipping empty ad placeholders

---
+++ tests/box_generation_ad_placeholders.rs
@@ -0,0 +1,28 @@
+use fastrender::css::parser::parse_stylesheet;
+use fastrender::style::cascade::apply_styles_with_media;
+use fastrender::style::media::MediaContext;
+use fastrender::tree::box_generation::generate_box_tree_with_anonymous_fixup;
+use fastrender::tree::box_tree::BoxNode;
+use fastrender::{dom, dom::DomNode};
+
+fn contains_class(node: &BoxNode, class: &str) -> bool {
+    if let Some(info) = &node.debug_info {
+        if info.classes.iter().any(|c| c == class) {
+            return true;
+        }
+    }
+    node.children.iter().any(|child| contains_class(child, class))
+}
+
+#[test]
+fn empty_ad_placeholders_are_dropped() {
+    let dom: DomNode = dom::parse_html(r#"<div class=\"ad-height-hold\"></div>"#).unwrap();
+    let stylesheet = parse_stylesheet("").unwrap();
+    let styled = apply_styles_with_media(&dom, &stylesheet, &MediaContext::screen(800.0, 600.0));
+
+    let box_tree = generate_box_tree_with_anonymous_fixup(&styled);
+    assert!(
+        !contains_class(&box_tree.root, "ad-height-hold"),
+        "empty ad placeholders should not generate boxes"
+    );
+}
