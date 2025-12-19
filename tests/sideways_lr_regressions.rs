a88934a5a952cbe8a862b67120dc6517370292ce
Wilson Lin <code@wilsonl.in>
2025-12-18 22:47:59 -0500

    Add border inline width mapping regression for sideways-lr

--- tests/sideways_lr_regressions.rs
+++ tests/sideways_lr_regressions.rs
@@ -174,3 +174,27 @@ fn border_inline_style_maps_in_sideways_lr_writing_mode() {
     assert_eq!(style.border_top_style, fastrender::style::types::BorderStyle::Dashed);
     assert_eq!(style.border_bottom_style, fastrender::style::types::BorderStyle::Dotted);
 }
+
+#[test]
+fn border_inline_width_maps_in_sideways_lr_writing_mode() {
+    let mut style = ComputedStyle::default();
+    style.writing_mode = WritingMode::SidewaysLr;
+    apply_declaration(
+        &mut style,
+        &decl(
+            "border-inline-width",
+            PropertyValue::Multiple(vec![
+                PropertyValue::Length(Length::px(2.0)),
+                PropertyValue::Length(Length::px(5.0)),
+            ]),
+        ),
+        &ComputedStyle::default(),
+        16.0,
+        16.0,
+    );
+    resolve_pending_logical_properties(&mut style);
+
+    // Sideways-lr inline axis is vertical -> start width maps to top, end width maps to bottom.
+    assert_eq!(style.border_top_width, Length::px(2.0));
+    assert_eq!(style.border_bottom_width, Length::px(5.0));
+}
