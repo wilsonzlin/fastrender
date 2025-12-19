7c458e2f4eafebbac7c781d4521a0d047ef0319e
Wilson Lin <code@wilsonl.in>
2025-12-18 23:54:31 -0500

    Add regression for out-of-range text-combine-upright digits

---
+++ tests/text_combine_upright_invalid_test.rs
@@ -0,0 +1,26 @@
+use fastrender::css::types::{Declaration, PropertyValue};
+use fastrender::style::properties::apply_declaration;
+use fastrender::style::types::TextCombineUpright;
+use fastrender::style::ComputedStyle;
+
+#[test]
+fn text_combine_upright_rejects_out_of_range_digits() {
+    let mut style = ComputedStyle::default();
+    apply_declaration(
+        &mut style,
+        &Declaration {
+            property: "text-combine-upright".into(),
+            value: PropertyValue::Multiple(vec![
+                PropertyValue::Keyword("digits".into()),
+                PropertyValue::Number(5.0),
+            ]),
+            raw_value: String::new(),
+            important: false,
+        },
+        &ComputedStyle::default(),
+        16.0,
+        16.0,
+    );
+
+    assert_eq!(style.text_combine_upright, TextCombineUpright::None);
+}
