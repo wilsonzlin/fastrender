5f6ea543fbbf410e71e608eb88889785d8d5c413
Wilson Lin <code@wilsonl.in>
2025-12-20 00:12:49 -0500

    Clippy: allow large enum variant on VarResolutionResult

--- src/style/var_resolution.rs
+++ src/style/var_resolution.rs
@@ -38,6 +38,7 @@ const MAX_RECURSION_DEPTH: usize = 10;
 
 /// Result of a var() resolution attempt
 #[allow(clippy::large_enum_variant)]
+#[allow(clippy::large_enum_variant)]
 #[derive(Debug, Clone)]
 pub enum VarResolutionResult {
     /// Successfully resolved to a value
