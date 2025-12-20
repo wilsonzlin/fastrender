c15ebc59109d49bd1b9833105606481bfee121fd
Wilson Lin <code@wilsonl.in>
2025-12-20 11:43:34 -0500

    revert: remove option_option allows

--- src/style/properties.rs
+++ src/style/properties.rs
@@ -9219,7 +9219,6 @@ fn parse_clip_path_value(value: &PropertyValue) -> Option<ClipPath> {
     }
 }
 
-#[allow(clippy::option_option)]
 fn parse_clip_value(value: &PropertyValue) -> Option<Option<ClipRect>> {
     match value {
         PropertyValue::Keyword(raw) => {
@@ -9677,7 +9676,6 @@ fn parse_text_decoration_style(value: &PropertyValue) -> Option<TextDecorationSt
     }
 }
 
-#[allow(clippy::option_option)]
 fn parse_text_decoration_color(value: &PropertyValue, current_color: Rgba) -> Option<Option<Rgba>> {
     match value {
         PropertyValue::Color(c) => Some(Some(c.to_rgba(current_color))),
@@ -9830,7 +9828,6 @@ fn parse_text_emphasis_style(value: &PropertyValue) -> Option<TextEmphasisStyle>
     }
 }
 
-#[allow(clippy::option_option)]
 fn parse_text_emphasis_color(value: &PropertyValue, current_color: Rgba) -> Option<Option<Rgba>> {
     match value {
         PropertyValue::Color(c) => Some(Some(c.to_rgba(current_color))),
@@ -9929,7 +9926,6 @@ fn parse_text_emphasis_position(value: &PropertyValue) -> Option<TextEmphasisPos
     }
 }
 
-#[allow(clippy::option_option)]
 fn parse_text_emphasis_shorthand(
     value: &PropertyValue,
     current_color: Rgba,
