5c596b5a559165564467dec37511b36c0853a0de
Wilson Lin <code@wilsonl.in>
2025-12-22 22:27:56 -0500

    Drop style variables test module reference

--- tests/style/mod.rs
+++ tests/style/mod.rs
@@ -21,7 +21,6 @@ mod media_test;
 mod order_property_test;
 mod root_font_size_percent_test;
 mod scope_rule_test;
-mod style_variables_test;
 mod supports_selector_test;
 mod supports_writing_mode_test;
 mod tab_size_test;
